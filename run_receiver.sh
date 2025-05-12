50e50fb4f4f5ff5da142a318c2adb34fc0bf7538
yuli821 <yuli9@illinois.edu>
2025-05-12 13:58:52 -0500

    testing without binding

--- run_receiver.sh
+++ run_receiver.sh
@@ -8,5 +8,6 @@ iface=${1:-ens3f1np1}
 results_dir=${2:-$DIR/results}
 
 for i in 28; do
-        $DIR/multi_stream_test.py $iface --config one-to-one --receiver --bind_app --cpus $i --throughput --utilisation --cache-miss --output $results_dir/one-to-one_${i} | tee $results_dir/one-to-one_${i}.log
+        # $DIR/multi_stream_test.py $iface --config one-to-one --receiver --bind_app --cpus $i --throughput --utilisation --cache-miss --output $results_dir/one-to-one_${i} | tee $results_dir/one-to-one_${i}.log
+        $DIR/multi_stream_test.py $iface --config one-to-one --receiver --cpus $i --throughput --utilisation --cache-miss --output $results_dir/one-to-one_${i} | tee $results_dir/one-to-one_${i}.log
 done
